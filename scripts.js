2fd46ea8a21477c475bf168064792bbe4a89cf8a
tashanyas <tashanyasmartin@gmail.com>
2023-10-29 14:47:57 -0500

    Edited scripts file

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
 /* Add your JavaScript to this file */
+
+onload=function(){
+    let email = document.querySelector('#email')
+    let message = document.querySelector('.message')
+    addEventListener('submit', (e) =>{
+        e.preventDefault()
+        if(!email.value == ''){
+            message.innerHTML = `Thank you! your email address ${email.value} has been added to our mailing list`
+            email.value = ''
+        }else{
+            message.innerHTML = 'Please enter a valid email address'
+            email.value = ''
+        }
+    })
+}
